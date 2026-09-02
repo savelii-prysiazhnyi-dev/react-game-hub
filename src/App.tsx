d47946d797373434371adf3493485b448ed2d423
savelii-prysiazhnyi-dev <savelii.prysiazhnyi.dev@gmail.com>
2026-09-02 21:13:30 +0300

    feat: build a responsive grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { Grid, GridItem } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold" hideBelow="lg">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
 
 export default App;
