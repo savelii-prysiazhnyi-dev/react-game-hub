41631eef6861488aa6a46ae068a2bfda1258480d
savelii-prysiazhnyi-dev <savelii.prysiazhnyi.dev@gmail.com>
2026-09-03 23:24:54 +0300

    feat(ui): add rating emojis, improve typography, and refine layout

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, Heading, HStack, Image } from '@chakra-ui/react';
 import type { Game } from '../hooks/useGames';
 import getCroppedImageUrl from '../services/image-url';
 import CriticScore from './CriticScore';
+import Emoji from './Emoji';
 import PlatformIconList from './PlatformIconList';
 
 interface Props {
@@ -13,13 +14,16 @@ const GameCard = ({ game }: Props) => {
     <Card.Root>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <Card.Body>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
-            platforms={game.parent_platforms.map(p => p.platform)}
+            platforms={game.parent_platforms?.map(p => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">
+          {game.name}
+          <Emoji rating={game.rating_top} />
+        </Heading>
       </Card.Body>
     </Card.Root>
   );
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -13,10 +13,10 @@ import { MdPhoneIphone } from 'react-icons/md';
 import type { Platform } from '../hooks/useGames';
 
 interface Props {
-  platforms: Platform[];
+  platforms?: Platform[];
 }
 
-const PlatformIconList = ({ platforms }: Props) => {
+const PlatformIconList = ({ platforms = [] }: Props) => {
   const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
