084a941526e024d30ab9aee1100159dba8c9f22e
savelii-prysiazhnyi-dev <savelii.prysiazhnyi.dev@gmail.com>
2026-09-03 23:39:58 +0300

    perf(data): bundle static genres and platforms to optimize initial load

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,24 @@
 import { createSystem, defaultConfig, defineConfig } from '@chakra-ui/react';
 
 const config = defineConfig({
-  theme: {},
+  theme: {
+    tokens: {
+      colors: {
+        gray: {
+          50: { value: '#f9f9f9' },
+          100: { value: '#ededed' },
+          200: { value: '#d3d3d3' },
+          300: { value: '#b3b3b3' },
+          400: { value: '#a0a0a0' },
+          500: { value: '#898989' },
+          600: { value: '#6c6c6c' },
+          700: { value: '#202020' },
+          800: { value: '#121212' },
+          900: { value: '#111' },
+        },
+      },
+    },
+  },
 });
 
 const theme = createSystem(defaultConfig, config);
